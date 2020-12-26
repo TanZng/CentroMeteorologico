25d1cc4675670036523bc2f4e48f2e995ac356a6
Tan <taniazuniga.mx@gmail.com>
2020-12-25 21:45:04 -0600

    Fix: urls.py
    se descomento el path accounts, pues este permite que los usuarios se loggen
    
    +climaApp\urls.py
    la vista detallada no está implementada

--- climaApp/urls.py
+++ climaApp/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.index, name='main'),
     path('tarjetas', views.TarjetaListView.as_view(), name='tarjeta_list'),  # para las templates
     path('tarjeta/<int:pk>/update', views.TarjetaUpdateView.as_view(success_url=reverse_lazy('climaApp:tarjeta_list')), name='tarjeta_update'),
-    path('tarjeta/<int:pk>', views.TarjetaDetailView.as_view(), name='tarjeta_detail'),
+    path('tarjeta/<int:pk>', views.TarjetaDetailView.as_view(), name='tarjeta_detail'), # Este no tiene un template asociado
     path('tarjeta/create', views.TarjetaCreateView.as_view(success_url=reverse_lazy('climaApp:tarjeta_list')), name='tarjeta_create'),
     path('tarjeta/<int:pk>/update', views.TarjetaUpdateView.as_view(success_url=reverse_lazy('climaApp:tarjeta_list')), name='tarjeta_update'),
     path('tarjeta/<int:pk>/delete', views.TarjetaDeleteView.as_view(success_url=reverse_lazy('climaApp:tarjeta_list')), name='tarjeta_delete'),
