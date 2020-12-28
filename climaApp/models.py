c1ad175943d959915f92ceab83156ecdb00dd52d
Tan <taniazuniga.mx@gmail.com>
2020-12-27 20:19:57 -0600

    Model Pais
    Se agrego el model pais y la ciudad ahora necesita un pais

--- climaApp/models.py
+++ climaApp/models.py
@@ -7,18 +7,37 @@ from django.contrib.auth.models import User
 
 class Tarjeta(models.Model):
     nombre = models.CharField(
-        max_length=200,
+        max_length=50,
         help_text='Ingrese un nombre para la tarjeta (e.g. Mi ciudad)',
-        validators=[MinLengthValidator(2, "Make must be greater than 1 character")]
+        validators=[MinLengthValidator(2, "El nombre debe tener más de 1 carácter")]
     )
+    pais = models.ForeignKey('Pais', on_delete=models.CASCADE, null=False)
     ciudad = models.ForeignKey('Ciudad', on_delete=models.CASCADE, null=False)
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.nombre
+
 
 class Ciudad(models.Model):
-    name = models.CharField(max_length=40)
+    nombre = models.CharField(max_length=30, unique=True)
+    pais = models.ForeignKey('Pais', on_delete=models.CASCADE, null=False)
 
     def __str__(self):
-        return self.name
+        return self.nombre
+
+    class Meta:
+        verbose_name_plural = "Ciudades"
+
+
+class Pais(models.Model):
+    nombre = models.CharField(max_length=30, unique=True)
+    iso = models.CharField(max_length=3, unique=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name_plural = "Paises"
